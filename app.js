f74d9480189b9b0036fa4d3d2a3a1515bda92f9b
Daniel Rodriguez <drodriguez661@gmail.com>
2021-03-29 11:14:08 -0500

    new start

---
+++ app.js
@@ -0,0 +1,8 @@
+//Search input
+const searchUser = document.getElementById('searchUser');
+
+//search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  //Get input text
+  const userText = e.target = e.target
+})
